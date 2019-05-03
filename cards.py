bc2a5a5082634a6ad43740244c6a3a4c18bebecb
koffes <koffes@none.none>
2019-05-03 21:20:59 +0200

    added cards file

---
+++ cards.py
@@ -0,0 +1,29 @@
+
+CARDS_NUM = 16
+SIDE_LEN = 4
+
+BPRT_T = 0
+BPRT_L = 1
+
+COL_G = 0
+COL_R = 1
+COL_P = 2
+COL_B = 3
+
+CARDS = [
+    [[COL_P, BPRT_L], [COL_G, BPRT_T], [COL_B, BPRT_T], [COL_R, BPRT_L]],
+    [[COL_G, BPRT_L], [COL_R, BPRT_T], [COL_P, BPRT_T], [COL_B, BPRT_L]],
+    [[COL_B, BPRT_L], [COL_R, BPRT_T], [COL_P, BPRT_T], [COL_G, BPRT_L]],
+    [[COL_P, BPRT_L], [COL_B, BPRT_T], [COL_R, BPRT_T], [COL_G, BPRT_L]],
+    [[COL_B, BPRT_L], [COL_P, BPRT_T], [COL_G, BPRT_T], [COL_R, BPRT_L]],
+    [[COL_P, BPRT_L], [COL_R, BPRT_T], [COL_B, BPRT_T], [COL_R, BPRT_L]],
+    [[COL_B, BPRT_L], [COL_G, BPRT_T], [COL_G, BPRT_T], [COL_R, BPRT_L]],
+    [[COL_P, BPRT_L], [COL_G, BPRT_T], [COL_B, BPRT_T], [COL_R, BPRT_L]],
+    [[COL_P, BPRT_L], [COL_R, BPRT_T], [COL_B, BPRT_T], [COL_G, BPRT_L]],
+    [[COL_G, BPRT_L], [COL_B, BPRT_T], [COL_P, BPRT_T], [COL_R, BPRT_L]],
+    [[COL_G, BPRT_L], [COL_G, BPRT_T], [COL_P, BPRT_T], [COL_R, BPRT_L]],
+    [[COL_R, BPRT_L], [COL_R, BPRT_T], [COL_B, BPRT_T], [COL_B, BPRT_L]],
+    [[COL_B, BPRT_L], [COL_G, BPRT_T], [COL_P, BPRT_T], [COL_G, BPRT_L]],
+    [[COL_R, BPRT_L], [COL_P, BPRT_T], [COL_B, BPRT_T], [COL_G, BPRT_L]],
+    [[COL_G, BPRT_L], [COL_R, BPRT_T], [COL_P, BPRT_T], [COL_G, BPRT_L]],
+    [[COL_P, BPRT_L], [COL_G, BPRT_T], [COL_R, BPRT_T], [COL_R, BPRT_L]]]
